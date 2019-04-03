87996eac824143584fb4865e4ded9b3f064f0bcd
DavinFortinoT <49236397+DavinFortinoT@users.noreply.github.com>
2019-04-03 20:09:31 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
-$channelAccessToken = 'CHANNEL ACCESS TOKEN';
+$channelAccessToken = 'Fxfy0AznF6hI4gOJLqnWHFzYWOe5J4jQqUIOSmI+1iwD4L008fQ51AvMWYhPSozNGkz6oPkQooXGwMzMXcGy6WYaxd5Y3FpLrDbqLtNCxWxQZOeRjRQnegEfGESxsjvBbkTb1s/E/5mT3YrCIyjaTgdB04t89/1O/w1cDnyilFU=
+';
 
-$channelSecret = 'CHANNEL SECRET';
+$channelSecret = '7ac37b8893ebedc085195fa38422de6a
+';
